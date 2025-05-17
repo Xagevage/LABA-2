58cfdb80f40703717f48228ff4d336e35daa10f2
Xagevage <149786533+Xagevage@users.noreply.github.com>
2025-05-17 10:31:02 +0300

    Add files via upload

---
+++ SyncVersion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace Lab2Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] urls = {
+                "https://jsonplaceholder.typicode.com/posts/1",
+                "https://api.spacexdata.com/v4/launches/latest",
+                "https://official-joke-api.appspot.com/random_joke"
+            };
+
+            HttpClient client = new HttpClient();
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Console.WriteLine("===== СИНХРОННЫЕ ЗАПРОСЫ =====");
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    Console.WriteLine($"\nЗапрос к {url}");
+                    var response = client.GetAsync(url).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine("Ответ:\n" + content);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Исключение: " + ex.Message);
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"\nОбщее время: {sw.ElapsedMilliseconds} мс");
+        }
+    }
+}
---
+++ aSyncVersion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lab2Async
+{
+    class aSyncVersion
+    {
+        static async Task Main(string[] args)
+        {
+            string[] urls = {
+                "https://jsonplaceholder.typicode.com/posts/1",
+                "https://api.spacexdata.com/v4/launches/latest",
+                "https://official-joke-api.appspot.com/random_joke"
+            };
+
+            HttpClient client = new HttpClient();
+            Stopwatch sw = Stopwatch.StartNew();
+
+            Console.WriteLine("===== АСИНХРОННЫЕ ЗАПРОСЫ =====");
+
+            foreach (var url in urls)
+            {
+                try
+                {
+                    Console.WriteLine($"\nЗапрос к {url}");
+                    var response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine("Ответ:\n" + content);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Исключение: " + ex.Message);
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"\nОбщее время: {sw.ElapsedMilliseconds} мс");
+        }
+    }
+}
